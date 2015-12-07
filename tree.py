cf0e32966d6d8aba809da2713d74b0138f0e3176
Victor Lei <vlei@g.harvard.edu>
2015-12-06 19:38:36 -0500

    Update tree hashing

--- tree.py
+++ tree.py
@@ -12,29 +12,17 @@ def bits(f):
         for i in reversed(xrange(8)):
             yield (b >> i) & 1
 
-f = open('filetest.pdf', 'r')
+f = open('50MB.bin', 'r')
 M = ''.join( map(str, [b for b in bits(f)]) )
 f.close()
 K = Keccak.Keccak(1600)
 
 
 def workProc(hashStr):
-    # Concatenate strings into current node, i
-#     print 'Current node:', i
     return K.Keccak((len(hashStr), hashStr))
 
-#         print len(tree[i]), hex(int(tree[i], 2))
-#         hexB = tree[i]
-
-#         print hexB, len(hexB)
-
-#         tree[i] = K.Keccak((len(tree[i]), hexB))
-#         print tree[i]
-
 def treeHash(MString, B = 1024, H = 2, D = 4):
-
     # Perform hashing at each layer and then concatenate
-
     L = D**H
     TS = sum([D**i for i in xrange(0, H+1)])
     curIdx = TS
@@ -45,38 +33,31 @@ def treeHash(MString, B = 1024, H = 2, D = 4):
         tree[len(tree) - i - 1] = []
 
     for i in xrange(0, len(M)):
-        if i%100000 == 0:
-            print i
+        # if i%100000 == 0:
+        #     print i
 
         j = int(i/B) % L
         tree[len(tree) - L + j].append(M[i])
 
-    curL = 0
-    # for i in xrange(0, len(M)/B):
-
-
     for i in xrange(0, L):
         curB = ''.join(tree[len(tree) - i - 1])
-        if len(curB) % B != 0:
+        if len(curB) % B != 0: # Add padding if not a multiple of B
             tree[len(tree) - i - 1] = (B - len(curB)) * '0' + curB
         else:
             tree[len(tree) - i - 1] = curB
 
-
     startT = time.time()
     for curL in range(H, -1, -1):
-        print curL
+        print 'Level', curL
         curN = D**curL
         curIdx -= curN
 
         # Hash nodes on this level in parallel
-
         p = Pool(4)
         for i in xrange(curIdx, curIdx + curN):
             if curL < H:
                 tree[i] = []
                 for j in xrange(i*D+1, i*D+(D+1)):
-    #                 print '    Adding', j
                     tree[i].append(tree[j])
 
                 tree[i] = ''.join(tree[i])
@@ -90,19 +71,17 @@ def treeHash(MString, B = 1024, H = 2, D = 4):
 print 'Final output'
 print '================='
 
+
 startT = time.time()
 tRet = treeHash(M)
 print tRet, len(tRet)
-
 print 'Time = ', time.time() - startT
 
+# Using normal Keccak function
 startT = time.time()
-# K = Keccak.Keccak(1600)
 # http://stackoverflow.com/questions/3470398/list-of-integers-into-string-byte-array-python
 BStr = ''.join( map(str,M) )
 hexB = "{0:0x}".format(int(BStr, 2))
-# hexB = hex(int(BStr,2))[2:-1]
-# print hexB
 res = K.Keccak( (len(M), hexB) )
 print res, len(res)*4
 print 'Time = ', time.time() - startT
