fe329e28d300ba36e9d3065fe4dd547fbd836a87
Victor Lei <vlei@g.harvard.edu>
2015-12-09 16:57:59 -0500

    Update tree hashing

--- tree.py
+++ tree.py
@@ -12,32 +12,39 @@ def bits(f):
         for i in reversed(xrange(8)):
             yield (b >> i) & 1
 
-f = open('50MB.bin', 'r')
+f = open('100KB.bin', 'r')
 M = ''.join( map(str, [b for b in bits(f)]) )
 f.close()
 K = Keccak.Keccak(1600)
 
+fM1, fM2 = None, None
 
 def workProc(hashStr):
-    return K.Keccak((len(hashStr), hashStr))
+    return K.Keccak((len(hashStr)*4, hashStr))
 
-def treeHash(MString, B = 1024, H = 2, D = 4):
+def treeHash(M, B = 1024, H = 2, D = 4):
     # Perform hashing at each layer and then concatenate
     L = D**H
     TS = sum([D**i for i in xrange(0, H+1)])
     curIdx = TS
     tree = [None for i in xrange(TS)]
+    print 'Message:', len(M)
 
     # Get message into the leaves of the tree
     for i in xrange(0, L):
         tree[len(tree) - i - 1] = []
 
-    for i in xrange(0, len(M)):
-        # if i%100000 == 0:
-        #     print i
+    # for i in xrange(0, len(M)):
+    #     # if i%100000 == 0:
+    #     #     print i
+    #
+    #     j = int(i/B) % L
+    #     tree[len(tree) - L + j].append(M[i])
 
-        j = int(i/B) % L
-        tree[len(tree) - L + j].append(M[i])
+    nGroups = len(M)/B + (len(M)%B != 0)
+    for i in xrange(0, nGroups):
+        tree[len(tree) - L + i%L].append(M[i*B: (i+1)*B])
+
 
     for i in xrange(0, L):
         curB = ''.join(tree[len(tree) - i - 1])
@@ -46,11 +53,14 @@ def treeHash(MString, B = 1024, H = 2, D = 4):
         else:
             tree[len(tree) - i - 1] = curB
 
-    startT = time.time()
+    startT2 = time.time()
     for curL in range(H, -1, -1):
         print 'Level', curL
         curN = D**curL
         curIdx -= curN
+
+        # if curL == 0:
+        #     print len(tree[0])
 
         # Hash nodes on this level in parallel
         p = Pool(4)
@@ -64,24 +74,31 @@ def treeHash(MString, B = 1024, H = 2, D = 4):
             else:
                 tree[i] = "{0:0{1}x}".format(int(tree[i], 2), len(tree[i])/4)
 
+        # if curL == 0:
+        #     global fM1
+        #     fM1 = tree[0]
         tree[curIdx: curIdx+curN] = p.map(workProc, tree[curIdx: curIdx+curN])
-    print time.time() - startT
+    print time.time() - startT2
     return tree[0]
 
 print 'Final output'
 print '================='
 
 
-startT = time.time()
-tRet = treeHash(M)
-print tRet, len(tRet)
-print 'Time = ', time.time() - startT
+for h in xrange(0, 5):
+    print 'Height:', h
+    startT = time.time()
+    tRet = treeHash(M, H=h)
+    print tRet, len(tRet)*4
+    print 'Time = ', time.time() - startT
 
 # Using normal Keccak function
-startT = time.time()
+
 # http://stackoverflow.com/questions/3470398/list-of-integers-into-string-byte-array-python
+startT = time.time()
 BStr = ''.join( map(str,M) )
 hexB = "{0:0x}".format(int(BStr, 2))
+# print 'Equal?', (hexB == fM1)
 res = K.Keccak( (len(M), hexB) )
 print res, len(res)*4
 print 'Time = ', time.time() - startT
